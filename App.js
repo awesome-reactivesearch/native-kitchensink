73592ff35a1e5b3a4e5e8f699ff24eeb6a68de35
dhruvdutt <dhruvdutt.jadhav@gmail.com>
2018-04-05 03:49:07 +0530

    Add drawer lock mode

--- App.js
+++ App.js
@@ -1,32 +1,62 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
-import { StackNavigator, DrawerNavigator } from 'react-navigation';
+import { View, Text, StyleSheet, StatusBar, TouchableOpacity } from 'react-native';
+import { StackNavigator, DrawerNavigator, DrawerItems } from 'react-navigation';
+import { Ionicons as Icon } from '@expo/vector-icons';
 
 const styles = StyleSheet.create({
-  container: { flex: 1, alignItems: 'center', justifyContent: 'center' }
+  container: { flex: 1, paddingTop: StatusBar.currentHeight },
+  innerContainer: { flex: 1, alignItems: 'center', justifyContent: 'center' },
+  header: { padding: 13, paddingTop: 0 },
+  customDrawer: { paddingTop: StatusBar.currentHeight },
+  customDrawerTouch: { paddingLeft: 13, paddingBottom: 5 },
+  customDrawerIcon: { paddingRight: 7 },
 })
 
 class HomeScreen1 extends React.Component {
+  static navigationOptions = ({ navigation }) => {
+    return {
+      drawerLockMode: 'locked-closed'
+    }
+  }
+  
   render() {
     return (
       <View style={styles.container}>
-        <Text>Home Screen 1</Text>
+        <View style={styles.header}>
+          <TouchableOpacity onPress={() => {
+            this.props.navigation.navigate('DrawerToggle');
+          }}>
+            <Icon name="md-menu" size={30} />
+          </TouchableOpacity>
+        </View>
+        <View style={styles.innerContainer}>
+          <Text>Home Screen 1</Text>
+        </View>
       </View>
     );
   }
 }
 
 class HomeScreen2 extends React.Component {
+  static navigationOptions = ({ navigation }) => {
+    return {
+      drawerLockMode: 'locked-closed'
+    }
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Text>Home Screen 2</Text>
+        <Text>Header</Text>
+        <View style={styles.innerContainer}>
+          <Text>Home Screen 2</Text>
+        </View>
       </View>
     );
   }
 }
 
-// const ComponentsStack = StackNavigator({
+// const ComponentsStack1 = StackNavigator({
 //   Home: {
 //     screen: HomeScreen1,
 //   },
@@ -44,6 +74,24 @@ class HomeScreen2 extends React.Component {
 //   },
 // });
 
+const CustomDrawerContentComponent = (props) => (
+  <View style={styles.customDrawer}>
+    <TouchableOpacity onPress={() => {
+      // TODO: find a way to toggle off inner drawer and toggle in outer drawer
+      props.navigation.navigate('DrawerToggle');
+
+      // navigate to Components1 main screen
+      // props.navigation.navigate('Components1');
+    }} style={styles.customDrawerTouch}>
+      <View style={{ flexDirection: 'row' }}>
+        <Icon name="ios-arrow-back" size={25} style={styles.customDrawerIcon} />
+        <Text>Back to Component1</Text>
+      </View>
+    </TouchableOpacity>
+    <DrawerItems {...props} />
+  </View>
+);
+
 // Drawer for Component1 containing one screen/component for each option
 const ComponentsDrawer1 = DrawerNavigator({
   C1_Option1: {
@@ -52,6 +100,8 @@ const ComponentsDrawer1 = DrawerNavigator({
   C1_Option2: {
     screen: HomeScreen1,
   },
+}, {
+  contentComponent: CustomDrawerContentComponent
 });
 
 // Drawer for Component2 containing one screen/component for each option
@@ -62,6 +112,8 @@ const ComponentsDrawer2 = DrawerNavigator({
   C2_Option2: {
     screen: HomeScreen2,
   },
+}, {
+  contentComponent: CustomDrawerContentComponent
 });
 
 // Root Drawer containing drawers for each components
