cf3ef48b30e8da70f5e76e3eb1929e0bf55754e4
dhruvdutt <dhruvdutt.jadhav@gmail.com>
2018-04-05 02:58:13 +0530

    Add sample for nested drawer navigation

--- App.js
+++ App.js
@@ -1,21 +1,81 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
+import { StackNavigator, DrawerNavigator } from 'react-navigation';
 
-export default class App extends React.Component {
+const styles = StyleSheet.create({
+  container: { flex: 1, alignItems: 'center', justifyContent: 'center' }
+})
+
+class HomeScreen1 extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+        <Text>Home Screen 1</Text>
       </View>
     );
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+class HomeScreen2 extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Home Screen 2</Text>
+      </View>
+    );
+  }
+}
+
+// const ComponentsStack = StackNavigator({
+//   Home: {
+//     screen: HomeScreen1,
+//   },
+//   Notifications: {
+//     screen: HomeScreen1,
+//   },
+// });
+
+// const ComponentsStack2 = StackNavigator({
+//   Home: {
+//     screen: HomeScreen2,
+//   },
+//   Notifications: {
+//     screen: HomeScreen2,
+//   },
+// });
+
+// Drawer for Component1 containing one screen/component for each option
+const ComponentsDrawer1 = DrawerNavigator({
+  C1_Option1: {
+    screen: HomeScreen1,
+  },
+  C1_Option2: {
+    screen: HomeScreen1,
   },
 });
+
+// Drawer for Component2 containing one screen/component for each option
+const ComponentsDrawer2 = DrawerNavigator({
+  C2_Option1: {
+    screen: HomeScreen2,
+  },
+  C2_Option2: {
+    screen: HomeScreen2,
+  },
+});
+
+// Root Drawer containing drawers for each components
+const RootDrawer = DrawerNavigator({
+  Components1: {
+    screen: ComponentsDrawer1,
+  },
+  Components2: {
+    screen: ComponentsDrawer2,
+  },
+});
+
+export default class App extends React.Component {
+  render() {
+    return <RootDrawer />;
+  }
+}
