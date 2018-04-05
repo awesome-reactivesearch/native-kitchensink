e5e396d8f9e88d34968b272b52f8b594cfe66088
dhruvdutt <dhruvdutt.jadhav@gmail.com>
2018-04-05 20:38:23 +0530

    Eslint, prettier setup

---
+++ .eslintrc.js
@@ -0,0 +1,67 @@
+module.exports = {
+	extends: ['airbnb', 'prettier', 'prettier/react'],
+	plugins: ['prettier'],
+	env: {
+		browser: true,
+		es6: true,
+	},
+	parser: 'babel-eslint',
+	rules: {
+		'react/jsx-filename-extension': [
+			'error',
+			{
+				extensions: ['.js', '.jsx'],
+			},
+		],
+		'prettier/prettier': [
+			'error',
+			{
+				singleQuote: true,
+			},
+		],
+		indent: [
+			2,
+			'tab',
+			{
+				SwitchCase: 1,
+				VariableDeclarator: 1,
+			},
+		],
+
+		'no-tabs': 0,
+		'no-underscore-dangle': 0,
+		'operator-linebreak': ['error', 'before'],
+		'prefer-destructuring': 0,
+		'no-console': [
+			'error',
+			{
+				allow: ['warn', 'error'],
+			},
+		],
+		'class-methods-use-this': 0,
+		'arrow-parens': 0,
+		'react/jsx-indent': [2, 'tab'],
+		'react/jsx-indent-props': [2, 'tab'],
+		'react/no-danger': 0,
+		'react/no-typos': 0,
+		'react/require-default-props': 0,
+		'react/no-unused-prop-types': 0,
+		'react/sort-comp': 0,
+		'jsx-a11y/label-has-for': [
+			2,
+			{
+				components: ['Label'],
+				required: {
+					every: ['id'],
+				},
+				allowChildren: false,
+			},
+		],
+		'jsx-a11y/anchor-is-valid': 0,
+		'jsx-a11y/alt-text': 0,
+		'react/no-array-index-key': 0,
+		'no-eval': 0,
+		'react/prefer-stateless-function': 0,
+		'react/no-multi-comp': 0,
+	},
+};
