1c9acb0825b10ce642de893bdc7373830edbad1a
dhruvdutt <dhruvdutt.jadhav@gmail.com>
2018-04-09 10:59:14 +0530

    Add SingleDropdownRange component

---
+++ src/components/SingleDropdownRange/index.js
@@ -0,0 +1,89 @@
+import React from 'react';
+import { View, StyleSheet, StatusBar } from 'react-native';
+
+import SingleDropdownRange from './SingleDropdownRangeView';
+import { DEFAULT_COLORS } from './../helpers';
+
+const styles = StyleSheet.create({
+	headerSpacer: {
+		paddingTop: StatusBar.currentHeight,
+		backgroundColor: DEFAULT_COLORS.primary,
+	},
+});
+
+export const Basic = ({ navigation }) => (
+	<View style={{ flex: 1 }}>
+		<View style={styles.headerSpacer} />
+		<SingleDropdownRange
+			navigate={() => {
+				navigation.goBack();
+			}}
+		/>
+	</View>
+);
+
+export const WithoutPlaceholder = ({ navigation }) => (
+	<View style={{ flex: 1 }}>
+		<View style={styles.headerSpacer} />
+		<SingleDropdownRange
+			placeholder=""
+			navigate={() => {
+				navigation.goBack();
+			}}
+		/>
+	</View>
+);
+
+export const WithDefaultSelected = ({ navigation }) => (
+	<View style={{ flex: 1 }}>
+		<View style={styles.headerSpacer} />
+		<SingleDropdownRange
+			defaultSelected="Rating 3 to 4"
+			navigate={() => {
+				navigation.goBack();
+			}}
+		/>
+	</View>
+);
+
+export const WithCustomStyles = ({ navigation }) => (
+	<View style={{ flex: 1 }}>
+		<View style={styles.headerSpacer} />
+		<SingleDropdownRange
+			innerStyle={{
+				title: {
+					color: 'purple',
+				},
+				label: {
+					color: 'purple',
+				},
+			}}
+			navigate={() => {
+				navigation.goBack();
+			}}
+		/>
+	</View>
+);
+
+export const Playground = ({ navigation }) => (
+	<View style={{ flex: 1 }}>
+		<View style={styles.headerSpacer} />
+		<SingleDropdownRange
+			placeholder="Select a rating"
+			defaultSelected="Rating 3 to 4"
+			filterLabel="Book filter"
+			showFilter={false}
+			innerStyle={{
+				title: {
+					color: 'purple',
+				},
+				label: {
+					color: 'purple',
+				},
+			}}
+			navigate={() => {
+				navigation.goBack();
+			}}
+		/>
+	</View>
+);
