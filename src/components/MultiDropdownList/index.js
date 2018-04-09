5dc2f415442dc738e9c25d42574e2fd0a61aac5c
dhruvdutt <dhruvdutt.jadhav@gmail.com>
2018-04-09 10:58:50 +0530

    Add MultiDropdownList component

---
+++ src/components/MultiDropdownList/index.js
@@ -0,0 +1,145 @@
+import React from 'react';
+import { View, StyleSheet, StatusBar } from 'react-native';
+
+import MultiDropdownList from './MultiDropdownListView';
+import { DEFAULT_COLORS } from './../helpers';
+
+const styles = StyleSheet.create({
+	headerSpacer: {
+		paddingTop: StatusBar.currentHeight,
+		backgroundColor: DEFAULT_COLORS.primary,
+	},
+});
+
+export const Basic = ({ navigation }) => (
+	<View style={{ flex: 1 }}>
+		<View style={styles.headerSpacer} />
+		<MultiDropdownList
+			navigate={() => {
+				navigation.goBack();
+			}}
+		/>
+	</View>
+);
+
+export const WithPlaceholder = ({ navigation }) => (
+	<View style={{ flex: 1 }}>
+		<View style={styles.headerSpacer} />
+		<MultiDropdownList
+			placeholder=""
+			navigate={() => {
+				navigation.goBack();
+			}}
+		/>
+	</View>
+);
+
+export const WithSize = ({ navigation }) => (
+	<View style={{ flex: 1 }}>
+		<View style={styles.headerSpacer} />
+		<MultiDropdownList
+			size={6}
+			navigate={() => {
+				navigation.goBack();
+			}}
+		/>
+	</View>
+);
+
+export const WithCustomStyles = ({ navigation }) => (
+	<View style={{ flex: 1 }}>
+		<View style={styles.headerSpacer} />
+		<MultiDropdownList
+			innerStyle={{
+				title: {
+					color: 'purple',
+				},
+				label: {
+					color: 'purple',
+				},
+				checkbox: {
+					color: 'purple',
+				},
+			}}
+			navigate={() => {
+				navigation.goBack();
+			}}
+		/>
+	</View>
+);
+
+export const WithoutCount = ({ navigation }) => (
+	<View style={{ flex: 1 }}>
+		<View style={styles.headerSpacer} />
+		<MultiDropdownList
+			showCount={false}
+			navigate={() => {
+				navigation.goBack();
+			}}
+		/>
+	</View>
+);
+
+export const WithCustomSort = ({ navigation }) => (
+	<View style={{ flex: 1 }}>
+		<View style={styles.headerSpacer} />
+		<MultiDropdownList
+			sortBy="desc"
+			navigate={() => {
+				navigation.goBack();
+			}}
+		/>
+	</View>
+);
+
+export const WithSelectAll = ({ navigation }) => (
+	<View style={{ flex: 1 }}>
+		<View style={styles.headerSpacer} />
+		<MultiDropdownList
+			selectAllLabel="All Series"
+			navigate={() => {
+				navigation.goBack();
+			}}
+		/>
+	</View>
+);
+
+export const WithDefaultSelected = ({ navigation }) => (
+	<View style={{ flex: 1 }}>
+		<View style={styles.headerSpacer} />
+		<MultiDropdownList
+			defaultSelected={['Anita Blake', 'Discworld']}
+			navigate={() => {
+				navigation.goBack();
+			}}
+		/>
+	</View>
+);
+
+export const Playground = ({ navigation }) => (
+	<View style={{ flex: 1 }}>
+		<View style={styles.headerSpacer} />
+		<MultiDropdownList
+			size={10}
+			sortBy="count"
+			defaultSelected={['Anita Blake', 'Discworld']}
+			placeholder="Select multiple series"
+			showFilter={false}
+			filterLabel="Series filter"
+			innerStyle={{
+				title: {
+					color: 'purple',
+				},
+				label: {
+					color: 'purple',
+				},
+				checkbox: {
+					color: 'purple',
+				},
+			}}
+			navigate={() => {
+				navigation.goBack();
+			}}
+		/>
+	</View>
+);
