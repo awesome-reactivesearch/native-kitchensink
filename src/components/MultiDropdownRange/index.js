f2b5fd32c6671d81b008fb5b3577407beba71ef1
dhruvdutt <dhruvdutt.jadhav@gmail.com>
2018-04-09 10:58:59 +0530

    Add MultiDropdownRange component

---
+++ src/components/MultiDropdownRange/index.js
@@ -0,0 +1,90 @@
+import React from 'react';
+import { View, StyleSheet, StatusBar } from 'react-native';
+
+import MultiDropdownRange from './MultiDropdownRangeView';
+import { DEFAULT_COLORS } from './../helpers';
+
+const styles = StyleSheet.create({
+	headerSpacer: {
+		paddingTop: StatusBar.currentHeight,
+		backgroundColor: DEFAULT_COLORS.primary,
+	},
+});
+
+export const Basic = ({ navigation }) => (
+	<View style={{ flex: 1 }}>
+		<View style={styles.headerSpacer} />
+		<MultiDropdownRange
+			navigate={() => {
+				navigation.goBack();
+			}}
+		/>
+	</View>
+);
+
+export const WithoutPlaceholder = ({ navigation }) => (
+	<View style={{ flex: 1 }}>
+		<View style={styles.headerSpacer} />
+		<MultiDropdownRange
+			placeholder=""
+			navigate={() => {
+				navigation.goBack();
+			}}
+		/>
+	</View>
+);
+
+export const WithDefaultSelected = ({ navigation }) => (
+	<View style={{ flex: 1 }}>
+		<View style={styles.headerSpacer} />
+		<MultiDropdownRange
+			defaultSelected={['Rating 3 to 4']}
+			navigate={() => {
+				navigation.goBack();
+			}}
+		/>
+	</View>
+);
+
+export const WithCustomStyles = ({ navigation }) => (
+	<View style={{ flex: 1 }}>
+		<View style={styles.headerSpacer} />
+		<MultiDropdownRange
+			innerStyle={{
+				title: {
+					color: 'purple',
+				},
+				label: {
+					color: 'purple',
+				},
+				checkbox: {
+					color: 'purple',
+				},
+			}}
+			navigate={() => {
+				navigation.goBack();
+			}}
+		/>
+	</View>
+);
+
+export const Playground = ({ navigation }) => (
+	<View style={{ flex: 1 }}>
+		<View style={styles.headerSpacer} />
+		<MultiDropdownRange
+			defaultSelected={['Rating 3 to 4']}
+			placeholder="Select ratings"
+			innerStyle={{
+				title: {
+					color: 'purple',
+				},
+				label: {
+					color: 'purple',
+				},
+			}}
+			navigate={() => {
+				navigation.goBack();
+			}}
+		/>
+	</View>
+);
