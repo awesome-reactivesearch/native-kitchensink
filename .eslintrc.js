9f37f10ff7ef1c3d029541c6934c3d7aa69faa25
dhruvdutt <dhruvdutt.jadhav@gmail.com>
2018-04-12 01:25:41 +0530

    Update eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -67,6 +67,7 @@ module.exports = {
 		camelcase: 0,
 		'operator-linebreak': 0,
 		'import/no-extraneous-dependencies': 0,
+		'jsx-a11y/accessible-emoji': 0,
 		'react/prop-types': 0, // TODO: fix later
 	},
 };
