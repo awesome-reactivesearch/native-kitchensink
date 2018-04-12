d029ec01cb67a7c9c310a00e6a559c41dddd394e
dhruvdutt <dhruvdutt.jadhav@gmail.com>
2018-04-12 17:58:33 +0530

    Switch to new scoped approach to render child drawer items

--- src/__tests__/utils.test.js
+++ src/__tests__/utils.test.js
@@ -1,63 +1,66 @@
 import * as utils from '../utils';
 
-const inputMapping = {
-	DataSearch: {
-		DataSearch_withTitle: {
-			screen: 'DataSearch_withTitle',
-		},
-		DataSearch_withIcon: {
-			screen: 'DataSearch_withIcon',
-		},
+const input = [
+	{
+		key: 'Home',
+		params: undefined,
+		routeName: 'Home',
 	},
-	TextField: {
-		TextField_withTitle: {
-			screen: 'TextField_withTitle',
-		},
-		TextField_withIcon: {
-			screen: 'TextField_withIcon',
-		},
+	{
+		key: 'DataSearch_Basic',
+		params: undefined,
+		routeName: 'DataSearch_Basic',
 	},
-};
+	{
+		key: 'DataSearch_With Icon Position',
+		params: undefined,
+		routeName: 'DataSearch_With Icon Position',
+	},
+	{
+		key: 'DataSearch_Without Search Icon',
+		params: undefined,
+		routeName: 'DataSearch_Without Search Icon',
+	},
+	{
+		key: 'DataSearch_Playground',
+		params: undefined,
+		routeName: 'DataSearch_Playground',
+	},
+	{
+		key: 'TextField_Basic',
+		params: undefined,
+		routeName: 'TextField_Basic',
+	},
+	{
+		key: 'TextField_Without Placeholder',
+		params: undefined,
+		routeName: 'TextField_Without Placeholder',
+	},
+	{
+		key: 'TextField_Playground',
+		params: undefined,
+		routeName: 'TextField_Playground',
+	},
+	{
+		key: 'MultiDropdownList_Basic',
+		params: undefined,
+		routeName: 'MultiDropdownList_Basic',
+	},
+	{
+		key: 'MultiDropdownList_Without Placeholder',
+		params: undefined,
+		routeName: 'MultiDropdownList_Without Placeholder',
+	},
+];
 
-const outputTwo = {
-	DataSearch_withTitle: { screen: 'DataSearch_withTitle' },
-	DataSearch_withIcon: { screen: 'DataSearch_withIcon' },
-	TextField_withTitle: { screen: 'TextField_withTitle' },
-	TextField_withIcon: { screen: 'TextField_withIcon' },
-};
-
-const outputThree = {
-	DataSearch_withTitle: {
-		screen: 'DataSearch_withTitle',
-	},
-	DataSearch_withIcon: {
-		screen: 'DataSearch_withIcon',
-	},
-};
-
-const outputFour = {
-	TextField_withTitle: {
-		screen: 'TextField_withTitle',
-	},
-	TextField_withIcon: {
-		screen: 'TextField_withIcon',
-	},
+const output = {
+	DataSearch: { start: 1, end: 4 },
+	TextField: { start: 5, end: 7 },
+	MultiDropdownList: { start: 8, end: 9 },
 };
 
 /* eslint no-undef: 0 */
 
-test('Flattens ChildDrawer Objects', () => {
-	expect(utils.flattenChildDrawerObjects(inputMapping)).toEqual(outputTwo);
-});
-
-test('Fetches ChildDrawer Options for DataSearch', () => {
-	expect(utils.getChildDrawerOptions(inputMapping, 'DataSearch')).toEqual(
-		outputThree
-	);
-});
-
-test('Fetches ChildDrawer Options for TextField', () => {
-	expect(utils.getChildDrawerOptions(inputMapping, 'TextField')).toEqual(
-		outputFour
-	);
+test('Evaluates outer drawer list items', () => {
+	expect(utils.evaluateOuterDrawerListItems(input)).toEqual(output);
 });
